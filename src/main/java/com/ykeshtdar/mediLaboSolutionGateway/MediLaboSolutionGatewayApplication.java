100fe87a22ce190ffde5041f46920313054802ee
yas43 <ykeshtdar@yahoo.com>
2024-12-05 11:18:49 +0100

    initial commit

---
+++ src/main/java/com/ykeshtdar/mediLaboSolutionGateway/MediLaboSolutionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ykeshtdar.mediLaboSolutionGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLaboSolutionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLaboSolutionGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykeshtdar/mediLaboSolutionGateway/MediLaboSolutionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykeshtdar.mediLaboSolutionGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediLaboSolutionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
